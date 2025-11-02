519f8b6fc951500fe26188555661d4c2ac7d6d5d
Felipe Santos de Amorim <255582023@eniac.edu.br>
2025-11-02 20:19:25 -0300

    config inicial do projeto

---
+++ src/database/migrations/20251102223053-create-users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('usuarios', { 
+      id:{
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false
+      },
+      nome_usuario:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email_usuario:{
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      senha_usuario:{
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      created_at_usuario: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at_usuario: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('usuarios');
+  }
+};
